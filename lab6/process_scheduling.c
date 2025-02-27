93a1a9479b9dd4301111a713fa90b2b4ff4ceaf8
D vishwatej <141154035+vishwatejD@users.noreply.github.com>
2025-02-27 22:34:48 +0530

    Update process_scheduling.c

--- lab6/process_scheduling.c
+++ lab6/process_scheduling.c
@@ -5,18 +5,18 @@
 
 struct Process {
     int pid;
-    int bt; 
-    int at; 
-    int ct; 
-    int wt; 
-    int tat; 
+    int bt;
+    int at;
+    int ct;
+    int wt;
+    int tat;
     int priority;
 };
 
+// Function to calculate waiting and turnaround times
 void calculateTimes(struct Process processes[], int n) {
     int total_wt = 0, total_tat = 0;
-    
-   
+
     for (int i = 0; i < n; i++) {
         processes[i].tat = processes[i].ct - processes[i].at;
         processes[i].wt = processes[i].tat - processes[i].bt;
@@ -24,39 +24,48 @@ void calculateTimes(struct Process processes[], int n) {
         total_tat += processes[i].tat;
     }
 
-    printf("\nPID\tBurst Time\tArrival Time\tCompletion Time\tWaiting Time\tTurnaround Time\n");
+    printf("\nPID\tBT\tAT\tCT\tWT\tTAT\n");
     for (int i = 0; i < n; i++) {
-        printf("%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", processes[i].pid, processes[i].bt, processes[i].at, processes[i].ct, processes[i].wt, processes[i].tat);
+        printf("%d\t%d\t%d\t%d\t%d\t%d\n", processes[i].pid, processes[i].bt, processes[i].at, processes[i].ct, processes[i].wt, processes[i].tat);
     }
-    
-    printf("\nAverage Waiting Time: %.2f", (float)total_wt / n);
-    printf("\nAverage Turnaround Time: %.2f", (float)total_tat / n);
+
+    printf("\nAvg Waiting Time: %.2f", (float)total_wt / n);
+    printf("\nAvg Turnaround Time: %.2f\n", (float)total_tat / n);
 }
 
+// **Preemptive SJF Scheduling (Fixed)**
 void preemptiveSJF(struct Process processes[], int n) {
-    int total_time = 0;
-    int completed = 0;
-    int shortest = 0;
-    int min_bt = 9999;
+    int remaining_bt[MAX], total_time = 0, completed = 0;
+    int shortest, min_bt;
     int is_completed[MAX] = {0};
-    int last_completed_time = 0;
+
+    for (int i = 0; i < n; i++)
+        remaining_bt[i] = processes[i].bt;
 
     printf("\nPreemptive SJF Scheduling:\n");
 
     while (completed < n) {
+        shortest = -1;
+        min_bt = 9999;
+
         for (int i = 0; i < n; i++) {
-            if (processes[i].at <= total_time && !is_completed[i] && processes[i].bt < min_bt) {
-                min_bt = processes[i].bt;
+            if (processes[i].at <= total_time && !is_completed[i] && remaining_bt[i] < min_bt) {
+                min_bt = remaining_bt[i];
                 shortest = i;
             }
         }
 
-        processes[shortest].bt--;
-        if (processes[shortest].bt == 0) {
+        if (shortest == -1) {
+            total_time++;
+            continue;
+        }
+
+        remaining_bt[shortest]--;
+
+        if (remaining_bt[shortest] == 0) {
+            processes[shortest].ct = total_time + 1;
             is_completed[shortest] = 1;
             completed++;
-            processes[shortest].ct = total_time + 1;
-            min_bt = 9999;  
         }
 
         total_time++;
@@ -65,14 +74,13 @@ void preemptiveSJF(struct Process processes[], int n) {
     calculateTimes(processes, n);
 }
 
+// **Round Robin Scheduling**
 void roundRobin(struct Process processes[], int n, int quantum) {
-    int total_time = 0;
+    int total_time = 0, completed = 0;
     int remaining_bt[MAX];
-    int completed = 0;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         remaining_bt[i] = processes[i].bt;
-    }
 
     printf("\nRound Robin Scheduling:\n");
 
@@ -80,8 +88,8 @@ void roundRobin(struct Process processes[], int n, int quantum) {
         for (int i = 0; i < n; i++) {
             if (remaining_bt[i] > 0) {
                 if (remaining_bt[i] > quantum) {
-                    remaining_bt[i] -= quantum;
                     total_time += quantum;
+                    remaining_bt[i] -= quantum;
                 } else {
                     total_time += remaining_bt[i];
                     processes[i].ct = total_time;
@@ -95,17 +103,17 @@ void roundRobin(struct Process processes[], int n, int quantum) {
     calculateTimes(processes, n);
 }
 
+// **Non-Preemptive Priority Scheduling (Fixed)**
 void priorityScheduling(struct Process processes[], int n) {
-    int total_time = 0;
-    int completed = 0;
-    int highest_priority = 9999;
-    int process_index = -1;
+    int total_time = 0, completed = 0;
     int is_completed[MAX] = {0};
 
     printf("\nNon-Preemptive Priority Scheduling:\n");
 
     while (completed < n) {
-        highest_priority = 9999;
+        int highest_priority = 9999;
+        int process_index = -1;
+
         for (int i = 0; i < n; i++) {
             if (processes[i].at <= total_time && !is_completed[i] && processes[i].priority < highest_priority) {
                 highest_priority = processes[i].priority;
@@ -113,18 +121,37 @@ void priorityScheduling(struct Process processes[], int n) {
             }
         }
 
-        if (process_index != -1) {
-            processes[process_index].ct = total_time + processes[process_index].bt;
-            total_time += processes[process_index].bt;
-            is_completed[process_index] = 1;
-            completed++;
+        if (process_index == -1) {
+            total_time++;
+            continue;
         }
+
+        processes[process_index].ct = total_time + processes[process_index].bt;
+        total_time += processes[process_index].bt;
+        is_completed[process_index] = 1;
+        completed++;
     }
 
     calculateTimes(processes, n);
+}
 
+// **Main Function (Fixed)**
+int main() {
+    int n, choice, quantum;
+    struct Process processes[MAX];
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    printf("Enter process details (PID, AT, BT, Priority):\n");
+    for (int i = 0; i < n; i++) {
+        printf("Process %d:\n", i + 1);
+        printf("PID: ");
+        scanf("%d", &processes[i].pid);
         printf("Arrival Time: ");
         scanf("%d", &processes[i].at);
+        printf("Burst Time: ");
+        scanf("%d", &processes[i].bt);
         printf("Priority: ");
         scanf("%d", &processes[i].priority);
     }
@@ -133,7 +160,7 @@ void priorityScheduling(struct Process processes[], int n) {
         printf("\nMenu:\n");
         printf("1. Preemptive SJF Scheduling\n");
         printf("2. Round Robin Scheduling\n");
-        printf("3. Non-preemptive Priority Scheduling\n");
+        printf("3. Non-Preemptive Priority Scheduling\n");
         printf("4. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
